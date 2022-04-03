dadafa0d5f733e212ccaa3f2f4c16a01bae4dab3
hyundukseo <hyundukseo98@gmail.com>
2022-04-03 07:47:28 +0000

    [FEAT][hyundukseo] edited datasets file

--- MODELS/CF/AutoRec/datasets.py
+++ MODELS/CF/AutoRec/datasets.py
@@ -14,7 +14,7 @@ class AutoRecDataset(Dataset) :
         return len(self.user_item_matrix.iloc[0, :])
 
     def __getitem__(self, index) :
-
+        
         # index번째 유저 행 추출, 첫번째 값은 유저 id이기 때문에 제외하고 반환
         user_vector = torch.tensor(self.user_item_matrix.iloc[index, :].to_numpy()).float()
         return user_vector, user_vector
@@ -31,7 +31,6 @@ class InferenceDataset(Dataset) :
         return len(self.user_item_matrix.iloc[0, :])
 
     def __getitem__(self, index) :
-
         # index번째 유저 행 추출, 첫번째 값은 유저 id이기 때문에 제외하고 반환
         user_vector = torch.tensor(self.user_item_matrix.iloc[index, :].to_numpy()).float()
         return user_vector, user_vector
