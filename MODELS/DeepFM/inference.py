a4b0143e3c534534db52a154776c15497c915b39
halucinor <halucinor0@gmail.com>
2022-04-10 13:04:41 +0000

    [feat][백승주] deepFM code update

--- MODELS/DeepFM/inference.py
+++ MODELS/DeepFM/inference.py
@@ -11,6 +11,7 @@ from models import *
 from datasets import *
 import tqdm
 from tqdm.auto import tqdm
+import json
 
 def load_model(saved_model, input_dims, device, args): # TODO : path define
     model_cls = getattr(import_module("models"), args.model)
@@ -77,19 +78,20 @@ def inference(args):
             # print (x)
             # print ("--------------------------")
             output = model(x) #[B] ///     item[idx] = x 에 대한 확률 output[idx]
+            # idx = torch.where(output >= 0.5)[0]
+            
+            # x = x.index_select(0,idx)
+            # output = output.index_select(0,idx)
+            
             preds = torch.cat((x,output.unsqueeze(1)),dim =1) # [B , 4]
             rating = torch.cat((rating, preds.cpu()), dim = 0)
     
     outputs = rating.numpy()
 
     info = []
-<<<<<<< HEAD
-    for user_id in range(n_users):
-=======
     #-- Select Top 10 items
     print ("[INFO] Select Top 10 Items..")
     for user_id in tqdm(range(n_users)):
->>>>>>> origin/feature/deepfm-attr-concat
         idx = np.where(outputs[:,0].astype(int) == user_id)
         user_rating = outputs[idx[0]]
         output_best10_idx = np.argpartition(user_rating[:,-1], -10)[-10:]
@@ -100,28 +102,43 @@ def inference(args):
             info.append([user,item])
         
     info = pd.DataFrame(info, columns=['user','item'])
-    info.to_csv(os.path.join(output_dir, "submission.csv"),index=False)
-
+    info.to_csv("./submission.csv",index=False)
     print("Inference Done!")
+
+    print("Testing recall@10...")
+
+    # 학습에 사용된 user만 uniq_user에 저장
+    train_ratings_df = pd.read_csv('/opt/ml/input/data/train/train_ratings.csv')
+    uniq_user = train_ratings_df['user'].unique()
+    print (f"Number of users : {len(uniq_user)}")
+
+    with open("/opt/ml/input/workspace/BERT4Rec/data/answers.json", "r") as json_file:
+        answer = json.load(json_file)
+
+    # movielens-20m과 submission을 비교하여 Recall@10 값 계산
+    submission_df = info # TODO: submission_file path에 맞게 수정!
+    recall_result = []
+
+    # 각 유저마다 recall@10 계산하여 list에 저장
+    for user in tqdm(uniq_user):
+        submission_by_user = submission_df[submission_df['user'] == user]['item']
+
+        hit = 0
+        for item in submission_by_user:
+            if item in answer[str(user)]:
+                hit += 1
+
+        recall_result.append(hit / 10)
+
+    # 전체 유저의 Recall@10의 평균 출력
+    print (f"Predicted submission result of Recall@10 = {np.average(recall_result)}")    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
     #Data
-<<<<<<< HEAD
-    parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--batch_size', type=int, default=4096, help='input batch size for validing (default: 1000)')
     parser.add_argument('--embedding_dim', type=int, default=20, help='embedding dimention(default: 10)')
-    parser.add_argument('--attr', type=str, default='director', help='embedding dimention(default: 10)')
-
-    parser.add_argument('--rating_dir', type=str, default='./data/train/rating.csv')
-    parser.add_argument('--attr_dir', type=str, default='./data/train/director.csv')
-    
-    #model parameters
-    parser.add_argument('--model', type=str, default='DeepFM', help='model type (default: DeepFM)')
-    parser.add_argument('--model_dir', type=str, default="./exp/experiment7", help='model pth directory')
-=======
-    parser.add_argument('--batch_size', type=int, default=32, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--embedding_dim', type=int, default=10, help='embedding dimention(default: 10)')
     parser.add_argument('--attr', type=str, default=["genre", "writer"], help='embedding dimention(default: 10)')
 
     parser.add_argument('--rating_dir', type=str, default='./data/train/rating.csv')
@@ -129,8 +146,7 @@ if __name__ == "__main__":
     
     #model parameters
     parser.add_argument('--model', type=str, default='DeepFM', help='model type (default: DeepFM)')
-    parser.add_argument('--model_dir', type=str, default="./exp/experiment3", help='model pth directory')
->>>>>>> origin/feature/deepfm-attr-concat
+    parser.add_argument('--model_dir', type=str, default="./exp/experiment", help='model pth directory')
     parser.add_argument('--drop_ratio', type=float, default=0.1)
 
     parser.add_argument('--output_dir', type=str, default="./output", help='output directory')
