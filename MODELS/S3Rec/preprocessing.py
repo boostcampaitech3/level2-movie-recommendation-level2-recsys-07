9db34a59f75084819f9c2e2fb8d3aba7ab1d585e
ryujongmoon <0917jong@gmail.com>
2022-04-11 07:42:13 +0000

    [feat][유종문] preprocessing add

--- MODELS/S3Rec/preprocessing.py
+++ MODELS/S3Rec/preprocessing.py
@@ -1,19 +1,23 @@
 import pandas as pd
-
+import json
 
 def main():
-    # genres_df = pd.read_csv("../data/train/genres.tsv", sep="\t")
-    # array, index = pd.factorize(genres_df["genre"])
-    # genres_df["genre"] = array
-    # genres_df.groupby("item")["genre"].apply(list).to_json(
-    #     "data/Ml_item2attributes.json"
-    # )
+    genres_df = pd.read_csv("../data/train/genres.tsv", sep="\t")
+    array, index = pd.factorize(genres_df["genre"])
+    genres_df["genre"] = array
+
     writers_df = pd.read_csv("../data/train/writers.tsv", sep="\t")
     array, index = pd.factorize(writers_df["writer"])
     writers_df["writer"] = array
-    writers_df.groupby("item")["writer"].apply(list).to_json(
-        "data/Ml_item2attributes.json"
-    )
 
+    genres_df_list = genres_df.groupby("item")["genre"].apply(list)
+    writers_df_list = writers_df.groupby("item")["writer"].apply(list)
+    
+    result = dict()
+    for item in writers_df_list.index:
+        result[item] = [genres_df_list[item], writers_df_list[item]]
+        
+    with open('./data/train/genre_writer.json','w') as f:
+        json.dump(result, f)
 if __name__ == "__main__":
     main()
